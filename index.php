fbc486072ca418f31d56cdeb82b8004c9893b57f
dragorillo <mendezmc293@gmail.com>
2021-10-20 22:21:03 -0400

    se montaron los archivos

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+
+<?php
+
+    /*
+    print "Bienvenidos al curso de PHP <br>"; //imprime y hace un salto de línea (<br>).
+    print "Hola Alumnes <br>"; #Saludo a alumnos
+    print "Hasta la próxima."; */
+
+    $nombre="Marcello";
+    $edad=19;
+    print "El nombre del usuario es $nombre <br>";
+    echo $nombre," ",$edad;
+
+?>
+</body>
+</html>
